48825574aaf916eee57c5f782d52689bf303b9b8
Tarun Singh <tarun.singh1120@gmail.com>
2024-06-29 22:48:47 +0530

    resolver

--- src/app/components/products/product-detail/product-detail.component.ts
+++ src/app/components/products/product-detail/product-detail.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { Product } from "../../../models/product.model";
-import { AuthService } from "../../../services/auth-services/auth.service";
-import { Subscription } from "rxjs";
-import { NgForm } from "@angular/forms";
-import { CartPageService } from "../../../services/cart-page.service";
-import { map, mergeMap } from "rxjs/operators";
-import { WishlistService } from "src/app/services/wishlist.service";
-import { ProductDataService } from "src/app/services/product-data.service";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Product} from '../../../models/product.model';
+import {AuthService} from '../../../services/auth-services/auth.service';
+import {Subscription} from 'rxjs';
+import {NgForm} from '@angular/forms';
+import {CartPageService} from '../../../services/cart-page.service';
+import {map, mergeMap} from 'rxjs/operators';
+import {WishlistService} from 'src/app/services/wishlist.service';
+import {ProductDataService} from 'src/app/services/product-data.service';
 
 @Component({
-  selector: "app-product-detail",
-  templateUrl: "./product-detail.component.html",
-  styleUrls: ["./product-detail.component.css"],
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
   product: Product = null;
@@ -33,8 +33,8 @@ export class ProductDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.product = this.aroute.snapshot.data["res"].p;
-    this.wishlisted = this.aroute.snapshot.data["res"].w;
+    this.product = this.aroute.snapshot.data['res'].p;
+    this.wishlisted = this.aroute.snapshot.data['res'].w;
 
     this.authS.User.subscribe((user) => {
       if (user) {
@@ -47,17 +47,17 @@ export class ProductDetailComponent implements OnInit {
   onSubmit(f: NgForm) {
     if (!this.auth || this.webmaster) {
       this.authS.logout();
-      this.router.navigate(["/auth"]);
+      this.router.navigate(['/auth']);
       return;
     }
     let quantity = f.value.quantity;
     this.cservice.addToCart(this.product, +quantity);
-    this.router.navigate(["/buying/cart"]);
+    this.router.navigate(['/buying/cart']);
   }
 
   addToWishList() {
     this.wlService.addToWishList(this.product).subscribe((res) => {
-      if (res === "Product added to Wishlist") {
+      if (res === 'Product added to Wishlist') {
         this.wlService.getWishList().subscribe(() => {
           this.wishlisted = true;
         });
@@ -66,14 +66,12 @@ export class ProductDetailComponent implements OnInit {
   }
 
   removeFromWishList() {
-    this.wlService
-      .removeFromWishList(this.product.productId)
-      .subscribe((res) => {
-        if (res === "Product removed from wishlist") {
-          this.wlService.getWishList().subscribe(() => {
-            this.wishlisted = false;
-          });
-        }
-      });
+    this.wlService.removeFromWishList(this.product.productId).subscribe((res) => {
+      if (res === 'Product removed from wishlist') {
+        this.wlService.getWishList().subscribe(() => {
+          this.wishlisted = false;
+        });
+      }
+    });
   }
 }
